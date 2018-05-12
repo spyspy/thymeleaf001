acaa01eb9a092d6ec646849a4d8cc38491b0d79a
spyspy <spyhopping101@gmail.com>
2018-05-12 22:58:57 +0800

    v3

--- src/main/java/com/thymeleaf001/thymeleaf001/controller/Thymeleaf001Controller.java
+++ src/main/java/com/thymeleaf001/thymeleaf001/controller/Thymeleaf001Controller.java
@@ -1,32 +1,74 @@
 package com.thymeleaf001.thymeleaf001.controller;
 
+import com.thymeleaf001.thymeleaf001.models.HelloMessage;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.thymeleaf.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class Thymeleaf001Controller {
 
+
+
     @RequestMapping("/")
-    public String getIndex(ModelMap modelMap){
+    public String getIndex(ModelMap modelMap,Model model){
         modelMap.addAttribute("host","https://www.google.com/");
+        model.addAttribute("message","really good!!!");
+
         return "index";
         // Go To:  /resources/templates/index
     }
 
     @RequestMapping("/page2")
-    public String getPage2(ModelMap modelMap){
+    public String getPage2(ModelMap modelMap,Model model){
         modelMap.addAttribute("host","https://www.google.com/");
+
         return "page2";
         // Go To:  /resources/templates/index
     }
 
     //Only Show String on the Browser
-    @RequestMapping("/hello")
+    @RequestMapping(value="/hello", method= RequestMethod.GET)
     @ResponseBody
-    public String hello(){
-        return "hello world!!!!";
+    public String helloString(HttpServletRequest request, Model model){
+        String name = request.getParameter("name");
+
+        if(!StringUtils.isEmptyOrWhitespace(name)){
+            name = name+" !! ";
+        }else{
+            name = "";
+        }
+
+
+        return "<h3> Hello World!!!!  "+name+"</h3>";
     }
 
+
+    @RequestMapping(value = "/helloform",method=RequestMethod.GET)
+    public String helloForm(){
+        return "hello";
+    }
+
+    //Go to a html page
+    //Only Show String on the Browser
+    @RequestMapping(value="/helloform", method= RequestMethod.POST)
+    public String helloForm(HttpServletRequest request, Model model){
+        String name = request.getParameter("name");
+
+        if(!StringUtils.isEmptyOrWhitespace(name)){
+            name = name+" !! ";
+        }else{
+            name = "";
+        }
+
+        model.addAttribute("message", HelloMessage.getMessage(name));
+        //model.addAttribute("name",name);
+        return "hello";
+    }
 }
---
+++ src/main/java/com/thymeleaf001/thymeleaf001/models/HelloMessage.java
@@ -0,0 +1,7 @@
+package com.thymeleaf001.thymeleaf001.models;
+
+public class HelloMessage {
+    public static String getMessage(String name){
+        return "Hello......."+name+" !!??!!";
+    }
+}
